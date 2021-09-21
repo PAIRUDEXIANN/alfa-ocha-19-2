afc68ce878a1f2de456ed2f592dbc36547186d29
PAIRU <89888433+PAIRUDEXIANN@users.noreply.github.com>
2021-09-22 01:41:33 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,10 +18,10 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: 'たいけん',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'しんゆう',
           // q2: 'えええ',
           // q3: 'おおお'
         },
